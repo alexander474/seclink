bf8881c38e578ee4540c14cdd518074dfa7ea195
Erimarlie <erik.martin.lie@gmail.com>
2021-01-24 19:04:59 +0100

    Separate cmds for MobSF (Popen) and emulator (run)

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import settings
 import script_handler
 
 if __name__ == '__main__':
-    # script_handler.run(command=settings.MOBSF_RUN_CMD, path=settings.MOBSF_PATH, shell=True)
+    # MobSF Popen command
+    script_handler.popen(command=settings.MOBSF_RUN_CMD, path=settings.MOBSF_PATH, shell=True)
+    # Emulator run command
     script_handler.run(command=settings.EMULATOR_RUN_CMD, path=settings.EMULATOR_PATH, shell=True)
