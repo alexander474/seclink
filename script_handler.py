bbf2fda4311fd4c88e09910ec38427a008ce821f
Alexander Bredesen <alex474@hotmail.no>
2021-01-24 22:32:45 +0100

    made config for linux and windows

--- script_handler.py
+++ script_handler.py
@@ -2,31 +2,39 @@ import subprocess
 import shlex
 import settings
 import webbrowser
+import platform
+import os
 
 
 # https://stackoverflow.com/questions/29633719/subprocess-create-new-console (Check out for windows test)
 
 def run(command, path=settings.BASE_DIR, shell=True):
-    cmd = command  # shlex.split(command)
+    cmd = command
+    process = None
     print("CMD: ", cmd)
     print("PATH: ", path)
     if cmd:
-        # DETACHED_PROCESS flag to open separate terminal
-        process = subprocess.run(cmd, cwd=path, shell=shell, creationflags=subprocess.DETACHED_PROCESS)
+        # DETACHED_PROCESS flag to open separate terminal | preexec_fn=os.setpgrp for Linux
+        if platform.system() == settings.LINUX:
+            process = subprocess.run(cmd, cwd=path, shell=shell, preexec_fn=os.setpgrp)
+        elif platform.system() == settings.WINDOWS:
+            process = subprocess.run(cmd, cwd=path, shell=shell, creationflags=subprocess.DETACHED_PROCESS)
         print("ARGS: ", process.args)
-        
+    return process
+
 
 def popen(command, path=settings.BASE_DIR, shell=True):
-    cmd = command  # shlex.split(command)
+    cmd = command
+    process = None
     print("CMD: ", cmd)
     print("PATH: ", path)
     if cmd:
-        # DETACHED_PROCESS flag to open separate terminal
-        process = subprocess.Popen(cmd, cwd=path, shell=shell, creationflags=subprocess.DETACHED_PROCESS)
+        # DETACHED_PROCESS flag to open separate terminal | preexec_fn=os.setpgrp for Linux
+        if platform.system() == settings.LINUX:
+            process = subprocess.Popen(cmd, cwd=path, shell=shell, preexec_fn=os.setpgrp)
+        elif platform.system() == settings.WINDOWS:
+            process = subprocess.Popen(cmd, cwd=path, shell=shell, creationflags=subprocess.DETACHED_PROCESS)
         print("ARGS: ", process.args)
-        # Opens webbrowser
+        # Opens in browser
         webbrowser.open(f"http://{settings.MOBSF_IP}:{settings.MOBSF_PORT}", autoraise=True)
-
-#def open_browser(ip, port):
-    #print(ip, port)
-    #webbrowser.open(f"http://{ip}:{port}", autoraise=True)
+    return process
