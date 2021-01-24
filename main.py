7d090a064573f2fb111e7d8dad24de1b7a13d286
Alexander Bredesen <alex474@hotmail.no>
2021-01-24 17:15:18 +0100

    added mobsf launch

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import settings
 import script_handler
 
 if __name__ == '__main__':
-    print(settings.BASE_DIR)
-    script_handler.run(command=settings.MOBSF_RUN_CMD, path=settings.MOBSF_PATH, shell=True)
+    # script_handler.run(command=settings.MOBSF_RUN_CMD, path=settings.MOBSF_PATH, shell=True)
+    script_handler.run(command=settings.EMULATOR_RUN_CMD, path=settings.EMULATOR_PATH, shell=True)
--- script_handler.py
+++ script_handler.py
@@ -6,7 +6,9 @@ import settings
 # https://stackoverflow.com/questions/29633719/subprocess-create-new-console (Check out for windows test)
 
 def run(command, path=settings.BASE_DIR, shell=True):
-    cmd = shlex.split(command)
-    print(cmd, path, shell)
+    cmd = command  # shlex.split(command)
+    print("CMD: ", cmd)
+    print("PATH: ", path)
     if cmd:
-        subprocess.Popen(cmd, cwd=path, shell=shell)
+        process = subprocess.Popen(cmd, cwd=path, shell=shell)
+        print("ARGS: ", process.args)
