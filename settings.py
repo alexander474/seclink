fdcbbb221a8f88a8b288dd80da99b3fd1742bf6a
Alexander Bredesen <alex474@hotmail.no>
2021-01-24 16:15:47 +0100

    changed to OS spesific file run command

--- settings.py
+++ settings.py
@@ -14,9 +14,11 @@ MAC = "Darwin"
 MOBSF_PATH = "Documents/private/tools/Mobile-Security-Framework-MobSF"
 MOBSF_IP = "127.0.0.1"
 MOBSF_PORT = "8000"
-MOBSF_RUN_CMD = f"./run.sh {MOBSF_IP}:{MOBSF_PORT}"
+MOBSF_RUN_FILE = "./run.sh"
+MOBSF_RUN_CMD = f"{MOBSF_RUN_FILE} {MOBSF_IP}:{MOBSF_PORT}"
 
 # EMULATOR VARIABLES
 EMULATOR_PATH = "Android/Sdk/emulator"
 EMULATOR_AVD_NAME = "Nexus_6_API_28"
-EMULATOR_RUN_CMD = f"./emulator -avd {EMULATOR_AVD_NAME} -writable-system -no-snapshot"
+EMULATOR_RUN_FILE = "./emulator"
+EMULATOR_RUN_CMD = f"{EMULATOR_RUN_FILE} -avd {EMULATOR_AVD_NAME} -writable-system -no-snapshot"
